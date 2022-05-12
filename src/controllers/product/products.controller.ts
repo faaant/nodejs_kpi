569a0f4ae3acc2836bdf151f0e6574d98149c297
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-05-12 13:42:04 +0300

    Pretify code

--- src/controllers/product/products.controller.ts
+++ src/controllers/product/products.controller.ts
@@ -3,20 +3,20 @@ import { Request } from 'express';
 
 @Controller('products')
 export class ProductsController {
-    @Post()
-    createProduct(@Req() request: Request) {
-        return `This action create a product`;
-    }
+  @Post()
+  createProduct(@Req() request: Request) {
+    return `This action create a product`;
+  }
 
-    @Put(':id')
-    updateProduct(@Param() params, @Req() request: Request): string {
-        console.log(params.id);
-        return `This action update a #${params.id} product`;
-    }
+  @Put(':id')
+  updateProduct(@Param() params, @Req() request: Request): string {
+    console.log(params.id);
+    return `This action update a #${params.id} product`;
+  }
 
-    @Delete(':id')
-    deleteProduct(@Param() params): string {
-        console.log(params.id);
-        return `This action delete a #${params.id} product`;
-    }
+  @Delete(':id')
+  deleteProduct(@Param() params): string {
+    console.log(params.id);
+    return `This action delete a #${params.id} product`;
+  }
 }

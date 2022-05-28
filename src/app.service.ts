d78ea772a512bf767dc010e5559947d035d6352e
faaant <illiaeducation@gmail.com>
2022-05-28 21:04:48 +0300

    add 3rd party service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private httpService: HttpService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getRates(): Promise<any> {
+    const result = await firstValueFrom(
+      this.httpService.get(
+        'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/uah.json',
+      ),
+    );
+    return result;
   }
 }

2e7006666c053cdb66e01f17583f5e87140317ba
faaant <illiaeducation@gmail.com>
2022-05-30 21:07:31 +0300

    added Winston for logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.File({
+          filename:
+            'logs/Combined-' + new Date(Date.now()).toDateString() + '.log',
+          level: 'info',
+          handleExceptions: true,
+        }),
+        new winston.transports.File({
+          filename:
+            'logs/Errors-' + new Date(Date.now()).toDateString() + '.log',
+          level: 'error',
+        }),
+        new winston.transports.Console({
+          level: 'info',
+        }),
+      ],
+      exceptionHandlers: [
+        new winston.transports.File({ filename: 'logs/exceptions.log' }),
+      ],
+
+      format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.timestamp({
+          format: 'DD/MM/YYYY, HH:mm:ss',
+        }),
+        winston.format.printf(
+          (text) =>
+            `[Nest]  - ${[text.timestamp]}  [${text.context}] :  ${
+              text.level
+            }: ${text.message}`,
+        ),
+      ),
+    }),
+  });
+
   await app.listen(3000);
 }
 bootstrap();

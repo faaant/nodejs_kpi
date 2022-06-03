c0484c0f76a9cc07697a33f310c43619ec3ef7d7
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-06-03 15:43:07 +0300

    Added cookie check

--- src/middlewares/jwt-insertion.middleware.ts
+++ src/middlewares/jwt-insertion.middleware.ts
@@ -3,10 +3,12 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class JwtinsertionMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    const cookies = Object.fromEntries(
-      req.headers.cookie.split(';').map((el) => el.split('=')),
-    );
-    req.headers['authorization'] = `Bearer ${cookies['jwt']}`;
+    const cookies = req.headers?.cookie
+      ? Object.fromEntries(
+          req.headers.cookie.split(';').map((el) => el.split('=')),
+        )
+      : null;
+    req.headers['authorization'] = `Bearer ${cookies?.jwt}`;
     next();
   }
 }

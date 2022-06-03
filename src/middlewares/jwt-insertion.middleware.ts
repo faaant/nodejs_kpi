b93f286e5c130697632ddf0a90c4d0b77b42714e
faaant <illiaeducation@gmail.com>
2022-06-03 14:54:02 +0300

    change inserting token

--- src/middlewares/jwt-insertion.middleware.ts
+++ src/middlewares/jwt-insertion.middleware.ts
@@ -1,14 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { JWTTokenService } from '@shared/services/jwt-token.service';
 
 @Injectable()
 export class JwtinsertionMiddleware implements NestMiddleware {
-  constructor(private jwtTokenSerice: JWTTokenService) {}
   use(req: any, res: any, next: () => void) {
-    const idToken = this.jwtTokenSerice.getToken();
-    if (idToken) {
-      req.headers['authorization'] = `Bearer ${idToken}`;
-    }
+    const cookies = Object.fromEntries(
+      req.headers.cookie.split(';').map((el) => el.split('=')),
+    );
+    req.headers['authorization'] = `Bearer ${cookies['jwt']}`;
     next();
   }
 }

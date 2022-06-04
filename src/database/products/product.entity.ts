6523068418a5f01e3b09f6cbc43d2798c962bb20
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2022-06-04 15:46:53 +0300

    Added validation into product entity

--- src/database/products/product.entity.ts
+++ src/database/products/product.entity.ts
@@ -1,3 +1,4 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -6,23 +7,48 @@ export class Product {
   id?: string;
 
   @Column({ unique: true })
+  @IsNotEmpty({
+    message: 'Product name must be not empty',
+  })
   productName?: string;
 
   @Column()
+  @IsNotEmpty({
+    message: 'Price name must be not empty',
+  })
+  @IsNumber()
   price?: number;
 
   @Column('real')
+  @IsNotEmpty({
+    message: 'Weight must be not empty',
+  })
+  @IsNumber()
   weight?: number;
 
   @Column()
+  @IsNotEmpty({
+    message: 'Count must be not empty',
+  })
+  @IsNumber()
   count?: number;
 
   @Column()
+  @IsNotEmpty({
+    message: 'CPU must be not empty',
+  })
   CPU?: string;
 
   @Column()
+  @IsNotEmpty({
+    message: 'RAM must be not empty',
+  })
+  @IsNumber()
   RAM?: number;
 
   @Column()
+  @IsNotEmpty({
+    message: 'Resolution must be not empty',
+  })
   resolution?: string;
 }
